59f0ef1f87d033572d7b58d230b73fb420d8c1ca
Vivek <k.vivekk07@gmail.com>
2021-09-21 20:27:57 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  useEffect(() => {
+    setAdviceHandler();
+  }, []);
+
+  const setAdviceHandler = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        setAdvice(response.data.slip.advice);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="quote">
+        <h1>{advice}</h1>
+      </div>
+      <div className="btn__div">
+        <button className="btn" onClick={setAdviceHandler}>
+          Fetch quote
+        </button>
+      </div>
     </div>
   );
 }
